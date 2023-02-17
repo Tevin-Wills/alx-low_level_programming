503640dd53f4a570397468fa67dfe1585ff3d95b
Tevin-Wills <tevinlemalasia254@gmail.com>
2023-02-16 16:11:36 -0800

    C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-[200~#include<unistd.h>
+#include<unistd.h>
 /**
- *  * main - is entry point
- *   *
- *    * Return: 1, if succesful
- *     */
+ * main - is entry point
+ *
+ * Return: 1, if succesful
+ */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
