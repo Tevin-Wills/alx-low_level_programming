92a823f9e1387485ced225bf51ef8e0746cedf6a
Tevin-Wills <tevinlemalasia254@gmail.com>
2023-02-21 12:54:30 -0800

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ * main - prints _putchar
+ *
+ * Always returns 0 is success
+ */
+int main (void)
+{
+	_putchar(95); /*9s is the ASCII for _ */
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	
+	return (0);
+}
