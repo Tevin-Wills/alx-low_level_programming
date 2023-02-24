35ebbc2d229cc8593898dde029f995dc265ae459
Tevin-Wills <tevinlemalasia254@gmail.com>
2023-02-23 18:50:02 -0800

    prototype file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+/** Header file containting declarations
+ *for prototype functions used in the 0x04-more_functions_nested_loops
+ *task and directory
+ *Own library with declared functions
+ */
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
