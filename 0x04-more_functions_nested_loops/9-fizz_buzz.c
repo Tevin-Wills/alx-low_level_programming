e827abf05d8ad7a0f183f0baf472aeb61e63aab7
Tevin-Wills <tevinlemalasia254@gmail.com>
2023-02-24 03:49:29 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,25 +14,25 @@ int main(void)
 
 	for (num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && num % 5 == 0)
-		{
-			printf("Fizzbuzz");
-		}
-		else if (num % 3 == 0)
+		if (num % 3 == 0 && num % 5 != 0)
 		{
 			printf("Fizz");
 		}
-		else if (num % 5 == 0)
+		else if (num % 5 == 0 && num % 3 != 0)
 		{
 			printf("Buzz");
+		}
+		else if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (num == 1)
+		{
+			printf("%d", num);
 		}
 		else
 		{
 			printf("%d", num);
-		}
-		if (num != 100)
-		{
-			printf(" ");
 		}
 	}
 	printf("\n");
