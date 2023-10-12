8798ca65fd61f789ec5af0515ee8e403bf834add
Tevin-Wills <tevinlemalasia254@gmail.com>
2023-10-12 16:18:56 -0700

    a function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,13 +1,12 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
+
 /**
  * print_all - prints anything to the screen
  * @format: list of types of arguments passed to a function
  */
 void print_all(const char * const format, ...)
 {
-	int i, check_num; /*declare variables and va_arg datatype */
+	int i, check; /*declare variables and va_arg datatype */
 
 	char *str;
 	va_list spec;
@@ -20,28 +19,28 @@ void print_all(const char * const format, ...)
 		{
 			case 'i':
 				printf("%d", va_arg(spec, int));
-				check_num = 0; /*check if condition has been met */
+				check = 0; /*check if condition has been met */
 				break;
 			case 'f':
 				printf("%f", va_arg(spec, double));
-				check_num = 0;
+				check = 0;
 				break;
 			case 'c':
 				printf("%c", va_arg(spec, int));
-				check_num = 0;
+				check = 0;
 				break;
 			case 's':
 				str = va_arg(spec, char *);
 				if (str == NULL)
 					str = "(nil)";
 				printf("%s", str);
-				check_sum = 0;
+				check = 0;
 				break;
 			default:
-				check_num = 1;
+				check = 1;
 				break;
 		}
-		if (format[i + 1] != '\0' && check_num == 0) /* if NOT NULL */
+		if (format[i + 1] != '\0' && check == 0) /* if NOT NULL */
 			printf(", ");
 		i++; /* update step of iter var */
 	}
